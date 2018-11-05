15c076e9f3b70a9fd312e3930599a1dafc06cb3a
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-05 12:09:23 -0500

    adds functions to index.js that manipulates database with sql

--- index.js
+++ index.js
@@ -8,7 +8,52 @@ const db = pgp({
     database: 'node-todo-app-db'
 });
 
-db.any('select * from todos')
-    .then(results => {
-        console.log(results);
+// example of grabbing all the rows
+function getAll() {
+    return db.any('select * from todos')
+}
+// getAll()
+//     .then(results => {
+//         console.log(results);
+//         console.log('those were the todos');
+//     })
+
+// example of grabbing one row
+function getById(id){
+    return db.one(`select * from todos where id = $1`, [id])
+        .catch(err => {
+            // got nuthin
+            return {
+                name: 'no todo found'
+            };
+        })
+}
+getById(2)
+    .then(result => {
+        console.log(result);
     })
+// getById(200000)
+//     .then(result => {
+//         console.log(result);
+//     })
+
+// example of adding a row
+function add(name, completed) {
+    return db.one(`insert into todos (name, completed)
+        values
+            ($1, $2)
+        returning id
+    `, [name, completed]) 
+}
+add('walk chewbacca', false)
+    .catch(err => {
+        console.log(err);
+    })
+    .then(result => {
+        console.log(result);
+    })
+
+// example of updating a row
+
+
+// example of deleting a row
