0c1a38b06721d56622ba3415d1340181c5bc11f3
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-05 12:26:58 -0500

    changes to safter interpolation and structures it to visualize CRUD

--- index.js
+++ index.js
@@ -8,6 +8,24 @@ const db = pgp({
     database: 'node-todo-app-db'
 });
 
+// CREATE
+// example of adding a row
+function add(name, completed) {
+    return db.one(`insert into todos (name, completed)
+        values
+            ($1, $2)
+        returning id
+    `, [name, completed]) 
+}
+// add('walk chewbacca', false)
+//     .catch(err => {
+//         console.log(err);
+//     })
+//     .then(result => {
+//         console.log(result);
+//     })
+
+// RETRIEVE
 // example of grabbing all the rows
 function getAll() {
     return db.any('select * from todos')
@@ -28,32 +46,26 @@ function getById(id){
             };
         })
 }
-getById(2)
-    .then(result => {
-        console.log(result);
-    })
+// getById(2)
+//     .then(result => {
+//         console.log(result);
+//     })
 // getById(200000)
 //     .then(result => {
 //         console.log(result);
 //     })
 
-// example of adding a row
-function add(name, completed) {
-    return db.one(`insert into todos (name, completed)
-        values
-            ($1, $2)
-        returning id
-    `, [name, completed]) 
-}
-add('walk chewbacca', false)
-    .catch(err => {
-        console.log(err);
-    })
-    .then(result => {
-        console.log(result);
-    })
-
-// example of updating a row
-
-
+// UPDATE
 // example of deleting a row
+
+// DELETE
+// example of updating a row
+function deleteById(id){
+    return db.result(`delete from todos where id = $1`, [id])
+        
+}
+// deleteById(10)
+//     .then(result => {
+//         console.log(result.rowCount);
+//     })
+
