c5b25146fae3736ea4a11c28fd567cdc07b4fc55
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-05 11:08:33 -0500

    holds example select *

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
-// console.log(`hello ${personName}`);
 
-console.log(`that should not have worked correctly`);
+// pulls in a function from pg-promise
+// so it's followed by ();
+const pgp = require('pg-promise')();
+const db = pgp({
+    host: 'localhost',
+    port: 5432,
+    database: 'node-todo-app-db'
+});
+
+db.any('select * from todos')
+    .then(results => {
+        console.log(results);
+    })
