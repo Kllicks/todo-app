7b9505de3c528bfc2e517d5bc1a91dbfff9d5e5a
Kyle Sekellick <sekellickkyle@yahoo.com>
2018-11-07 10:05:03 -0500

    adds first attempt at Todo class with proper methods and contructor

--- models/Todo.js
+++ models/Todo.js
@@ -1,83 +1,151 @@
 const db = require('./db');
 
+class Todo {
+    constructor(id, name, completed) {
+        this.id = id;
+        this.name = name;
+        this.completed = completed;
+    }
+    
+    // CREATE
+    add(name, completed) {
+        return db.one(`insert into todos (name, completed)
+            values
+                ($1, $2)
+            returning id
+        `, [this.name, this.completed]) 
+    }
+
+    // RETRIEVE
+    getAll() {
+        return db.any('select * from todos')
+    }
+
+    static getById(id){
+        return db.one(`select * from todos where id = $1`, [id])
+            .then(result => {
+                const u = new Todo(result.id, result.name)
+                return u;
+            })
+    }
+
+    // UPDATE
+    assignToUser(id, userId) {
+        return db.result(`
+            update todos
+                set user_id = $2
+            where id = $1
+            `, [this.id, User.id]);
+    }
+
+    updateName(id, name) {
+        return db.result(`update todos
+                            set name=$2
+                            where id=$1`, [this.id, name]);
+    }
+
+    updateCompleted(id, didComplete) {
+        return db.result(`update todos 
+                            set completed=$2 
+                            where id=$1`, [this.id, didComplete]);
+    }
+    
+    static markCompleted(id) {
+        return updateCompleted(id, true);
+    }
+
+    static markPending(id) {
+        return updateCompleted(id, false);
+    }
+
+    deleteById(id){
+        return db.result(`delete from todos where id = $1`, [id]);
+    }
+
+    // DELETE
+
+    
+}
+
+
 // CREATE
 // example of adding a row
-function add(name, completed) {
-    return db.one(`insert into todos (name, completed)
-        values
-            ($1, $2)
-        returning id
-    `, [name, completed]) 
-}
+// function add(name, completed) {
+//     return db.one(`insert into todos (name, completed)
+//         values
+//             ($1, $2)
+//         returning id
+//     `, [name, completed]) 
+// }
 
 // RETRIEVE
 // example of grabbing all the rows
-function getAll() {
-    return db.any('select * from todos')
-}
+// function getAll() {
+//     return db.any('select * from todos')
+// }
 
 // example of grabbing one row
-function getById(id){
-    return db.one(`select * from todos where id = $1`, [id])
-        .catch(err => {
-            // got nuthin
-            return {
-                name: 'no todo found'
-            };
-        })
-}
+// function getById(id){
+//     return db.one(`select * from todos where id = $1`, [id])
+//         .catch(err => {
+//             // got nuthin
+//             return {
+//                 name: 'no todo found'
+//             };
+//         })
+// }
 
 // UPDATE
-function assignToUser(todoId, userId) {
-    return db.result(`
-        update todos
-            set user_id = $2
-        where id = $1
-        `, [todoId, userId]);
-}
+// function assignToUser(todoId, userId) {
+//     return db.result(`
+//         update todos
+//             set user_id = $2
+//         where id = $1
+//         `, [todoId, userId]);
+// }
 
 // example of updating a row
-function updateName(id, name) {
-    return db.result(`update todos
-                        set name=$2
-                        where id=$1`, [id, name]);
-}
+// function updateName(id, name) {
+//     return db.result(`update todos
+//                         set name=$2
+//                         where id=$1`, [id, name]);
+// }
 
 
-function updateCompleted(id, didComplete) {
-    return db.result(`update todos 
-	                    set completed=$2 
-	                    where id=$1`, [id, didComplete]);
-}
+// function updateCompleted(id, didComplete) {
+//     return db.result(`update todos 
+// 	                    set completed=$2 
+// 	                    where id=$1`, [id, didComplete]);
+// }
 
 // example of updating a row
-function markCompleted(id) {
-    return updateCompleted(id, true);
-    // return db.result(`update todos 
-	//                     set completed=$2 
-	//                     where id=$1`, [id, true]);
-}
-function markPending(id) {
-    return updateCompleted(id, false);
-    // return db.result(`update todos 
-    //                     set completed=$2 
-    //                     where id=$1`, [id, false]);
-}
+// function markCompleted(id) {
+//     return updateCompleted(id, true);
+//     // return db.result(`update todos 
+// 	//                     set completed=$2 
+// 	//                     where id=$1`, [id, true]);
+// }
+// function markPending(id) {
+//     return updateCompleted(id, false);
+//     // return db.result(`update todos 
+//     //                     set completed=$2 
+//     //                     where id=$1`, [id, false]);
+// }
 
 // DELETE
 // example of deleting a row
-function deleteById(id){
-    return db.result(`delete from todos where id = $1`, [id]);
-}
+// function deleteById(id){
+//     return db.result(`delete from todos where id = $1`, [id]);
+// }
 
-
-module.exports = {
-    add,
-    assignToUser,
-    getAll,
-    getById,
-    updateName,
-    markCompleted,
-    markPending,
-    deleteById,
-};
+module.exports = Todo;
+// module.exports = {
+//     add,
+//     assignToUser,
+//     getAll,
+//     getById,
+//     updateName,
+//     markCompleted,
+//     markPending,
+//     deleteById,
+// };
